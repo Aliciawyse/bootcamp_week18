887f855c76dcf2f20c2ece31113e200f1211e288
Barrett <Aliciawyse@users.noreply.github.com>
2018-01-07 08:47:53 -0500

    adding routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,13 @@
+//Server routes
+
+//Bing in the scrape function from our scripts directory
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require("../controllers/headlines");
+var notesController = require("..controllers/notes");
+
+
+
 module.exports = function(router){
     //this route renders the homepage
     router.get("/", function(req,res){
@@ -8,4 +18,42 @@ module.exports = function(router){
     router.get("/saved", function(req,res){
         res.render("saved");
     });
-}
+
+    router.get("/api/fetch", function(req,res){
+
+        headlinesController.fetch(function(err, doc){
+
+            if(!doc || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new articles today. Check back tomorrow."
+                });
+
+            } else{
+                res.json({
+                    message: "Added" + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+
+    //when router hits headlines, take what user requested and respond
+    //if user doesn't request something specific, respond with everything
+    router.get("/api/headlines", function(req,res){
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
+
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/headlines/:id", funcrion(req,res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    })
+};
